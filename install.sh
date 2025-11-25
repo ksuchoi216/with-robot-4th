f7c689d1e7617030a4ebcd955a2fbef26984b1fb
ksuchoi216 <ksuchoi216@gmail.com>
2025-11-25 09:54:42 +0900

    updated symbolic link.

---
+++ install.sh
@@ -0,0 +1,4 @@
+
+# Link assets folder
+rm -rf /Users/KC/Doing/with-robot-4th/model/robocasa/assets
+ln -s /Volumes/SSD/data/20201125_asset_for_robot/assets /Users/KC/Doing/with-robot-4th/model/robocasa/assets
